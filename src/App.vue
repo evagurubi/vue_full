5a17a0cb4169d010843e3489bc8d9d34dd373750
evagurubi <evagurubi@gmail.com>
2022-09-12 13:46:42 +0200

    First commit.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,94 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- <template>
+  <div class="firstattempt">
+
+    <h1>{{title}}</h1>
+    <input type="text" ref="name">
+   
+    <button @click="handleClick">CLICK ME</button> 
+    <p>Welcome...</p>
+   
+    <div v-if="showModal">
+      <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+    </div>
+    <button @click.alt="toggleModal">OPEN MODAL (alt)</button>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: "My First Vue App :)", 
+      header: "Sign up for the Giveaway!",
+      text:"Something silly", 
+      showModal: false
+    }
+  },
+  methods: {
+handleClick() {
+  console.log(this.$refs.name)
+  this.$refs.name.classList.add('active')
+  this.$refs.name.focus()
+}, 
+toggleModal() {
+  this.showModal = !this.showModal
+}
   }
+  
+}
+</script>
+
+********************************************************* -->
+
+<template>
+  <div class="firstattempt">
+
+    <h1>{{title}}</h1>
+    <input type="text" ref="name">
+   
+    <button @click="handleClick">CLICK ME</button> 
+    <p>Welcome...</p>
+   
+    <div v-if="showModal">
+      <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+    </div>
+    <button @click.alt="toggleModal">OPEN MODAL (alt)</button>
+  </div>
+  
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: "My First Vue App :)", 
+      header: "Sign up for the Giveaway!",
+      text:"Something silly", 
+      showModal: false
+    }
+  },
+  methods: {
+handleClick() {
+  console.log(this.$refs.name)
+  this.$refs.name.classList.add('active')
+  this.$refs.name.focus()
+}, 
+toggleModal() {
+  this.showModal = !this.showModal
+}
+  }
+  
 }
 </script>
 
@@ -22,5 +100,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.firstattempt {
+  border: 2px solid blue;
+  padding: 10px
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: block;
+  padding-bottom: 10px;
+
 }
 </style>
